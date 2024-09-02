442d383a99a2c5701eea8a79de30c6fade79323f
thievesshare <thievesshare@gmail.com>
2024-09-02 20:46:37 +0800

    feat : add 05

---
+++ 05.py
@@ -0,0 +1,12 @@
+
+list = ["A", "B", "CC"]
+
+# 對裡面的元素作判斷
+for item in list:
+    # 判斷是元素
+    if item == "B":
+       print("我是 B")
+    elif item == "CC":
+       print("我是 CC")
+    else:
+        print("我是剩下的Ａ")
